16ff1637134bf807fef9fc6c5e7bc0a25330424c
JihanLi <jh9228.li@gmail.com>
2017-11-07 23:44:47 -0800

    change path

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var port = process.env.PORT || 8080;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/favicons', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,7 +41,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error');
 });
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
 
 module.exports = app;
